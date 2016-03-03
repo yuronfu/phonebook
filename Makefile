6e4e1c91a68239ef17c00b23cfc6d95c22d82a9a
yuronfu <C14011073@mail.ncku.edu.tw>
2016-03-04 01:45:15 +0800

    first modify to phonebook_opt

--- Makefile
+++ Makefile
@@ -16,6 +16,7 @@ phonebook_orig: $(SRCS_common) phonebook_orig.c phonebook_orig.h
 phonebook_opt: $(SRCS_common) phonebook_opt.c phonebook_opt.h
 	$(CC) $(CFLAGS_common) $(CFLAGS_opt) \
 		-DIMPL="\"$@.h\"" -o $@ \
+		-DOPT="1"\
 		$(SRCS_common) $@.c
 
 run: $(EXEC)
--- phonebook_opt.h
+++ phonebook_opt.h
@@ -3,11 +3,8 @@
 
 #define MAX_LAST_NAME_SIZE 16
 
-/* TODO: After modifying the original version, uncomment the following
- * line to set OPT properly */
-// #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+#define OPT 1
+struct __ENTRY_DETAILS {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,6 +14,10 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+};
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    struct __ENTRY_DETAILS *detail;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
